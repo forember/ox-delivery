ccdaaebcb23a22ff58fda366dbe76ebdcc8c5a90
Chris McKinney <ntd5@tachibanatech.com>
2016-12-14 21:30:21 -0500

    Modified catkin_ws clean script to patch nav2d_karto.

--- catkin_ws/clean.sh
+++ catkin_ws/clean.sh
@@ -4,6 +4,13 @@ cd "$(dirname "$0")"
 
 ./fix_links.sh
 
+echo git submodule update --init --recursive
+git submodule update --init --recursive
+echo \<src/navigation_2d-git/src/nav2d_karto/CMakeLists.txt python -c 'import sys,re;sys.stdout.write(re.sub(r"(set\(Eigen3_INCLUDE_DIRS \$\{EIGEN3_INCLUDE_DIR\}\)\n)?catkin_package",r"set(Eigen3_INCLUDE_DIRS ${EIGEN3_INCLUDE_DIR})\ncatkin_package",sys.stdin.read(),1,re.M))' \>.sn2gsnkc.txt
+<src/navigation_2d-git/src/nav2d_karto/CMakeLists.txt python -c 'import sys,re;sys.stdout.write(re.sub(r"(set\(Eigen3_INCLUDE_DIRS \$\{EIGEN3_INCLUDE_DIR\}\)\n)?catkin_package",r"set(Eigen3_INCLUDE_DIRS ${EIGEN3_INCLUDE_DIR})\ncatkin_package",sys.stdin.read(),1,re.M))' >.sn2gsnkc.txt
+echo mv .sn2gsnkc.txt src/navigation_2d-git/src/nav2d_karto/CMakeLists.txt
+mv .sn2gsnkc.txt src/navigation_2d-git/src/nav2d_karto/CMakeLists.txt
+
 source /opt/ros/kinetic/setup.sh
 
 catkin_make clean
